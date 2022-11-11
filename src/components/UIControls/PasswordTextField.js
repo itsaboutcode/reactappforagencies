4db3e8d4a516bec408c23038c9aed8b902ca064b
LAPTOP-7AB1NSU2\ali raza <aliraza4300@gmail.com>
2022-11-11 18:21:18 +0500

    update eye icon

--- src/components/UIControls/PasswordTextField.js
+++ src/components/UIControls/PasswordTextField.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InputTypes from "./InputTypes";
-import ShowPasswordLabel from "./ShowPasswordLabel";
+import IconLabel from "./IconLabel";
+// import ShowPasswordLabel from "./ShowPasswordLabel";
 
 import { useState } from "react";
 
@@ -16,7 +17,10 @@ const PasswordTextField = ({ placeholder, id , })  =>  {
     return (
         <>
             <InputTypes inputType = {passwordShown ? "text" : "password"} inputClass = "form-control rounded-0" placeholder = { placeholder } inputId = { id } />
-            <button className="btn btn-empty p-0 border-0" onClick={togglePassword}><ShowPasswordLabel /></button>
+            <button className="btn btn-empty p-0 border-0" onClick={togglePassword}>
+            <IconLabel labelClass="input-group-text p-2" iconClass = { passwordShown ? "input-icon ic-eye-slash" : "input-icon  ic-eye-unslash" } />
+                {/* <ShowPasswordLabel /> */} 
+            </button>
         </>
     )
         
